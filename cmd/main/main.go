bfe88b6dba91a2fb548d13afdf2a22c6f302f387
CHINF <chinf1996@gmail.com>
2021-03-23 20:46:52 +0800

    嘗試同時控制22個馬達

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"kondocontrol/internal/convert"
 	"kondocontrol/internal/eeprom"
 	"log"
@@ -30,7 +28,7 @@ const (
 func main() {
 	// Set up options.
 	options := serial.OpenOptions{
-		PortName:          "COM7",
+		PortName:          "COM5",
 		BaudRate:          1250000,
 		DataBits:          8,
 		StopBits:          1,
@@ -38,26 +36,35 @@ func main() {
 		ParityMode:        serial.PARITY_EVEN,
 		RTSCTSFlowControl: false,
 	}
-
+	options2 := options
+	options2.PortName = "COM7"
 	// Open the port.
 	port, err := serial.Open(options)
 	if err != nil {
 		log.Fatalf("serial.Open: %v", err)
 	}
-
+	port2, err := serial.Open(options2)
+	if err != nil {
+		log.Fatalf("serial.Open2: %v", err)
+	}
 	// Make sure to close it later.
 	defer port.Close()
-
+	defer port2.Close()
 	{ // test readEEPROM
 		// r := readEEPROM(0, scEEPROM, port)
 		// fmt.Println(printHex(r), len(r))
 	}
 
 	{ // test setPosition and Free mode
-		// trySetPosition(port)
-		// tryFree(port)
+		go func() {
+			trySetPosition(port)
+			tryFree(port)
+		}()
+
+		trySetPosition(port2)
+		tryFree(port2)
 		// speedValue := uint8(50)
-		// for i := 0; i <= 10; i++ {
+		// for i := 0; i <= 3; i++ {
 		// 	writeEEPROM(uint8(i), scSpeed, []byte{speedValue}, port)
 		// }
 	}
@@ -71,20 +78,43 @@ func main() {
 		// fmt.Println(printHex(dat))
 	}
 	{ // convert data address to json
-		testingFilePath := "./Ignore/data"
-		dat, err := ioutil.ReadFile(testingFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		ee, err := eeprom.Parsing(dat)
-		if err != nil {
-			log.Fatalf("%+v\n", err)
-		}
-		j, err := json.Marshal(ee.Address)
-		if err != nil {
-			log.Fatalf("json.Marshal: %v", err)
-		}
-		fmt.Println(string(j))
+		// var testingFilePath string = "./Ignore/data"
+		// dat, err := ioutil.ReadFile(testingFilePath)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// ee, err := eeprom.Parsing(dat)
+		// if err != nil {
+		// 	log.Fatalf("%+v\n", err)
+		// }
+		// j, err := json.Marshal(ee.Address)
+		// if err != nil {
+		// 	log.Fatalf("json.Marshal: %v", err)
+		// }
+		// fmt.Println(string(j))
+	}
+
+	{ // Write EEPROM SignalSpeed to Low
+
+		// for id := uint8(0); id <= 10; id++ {
+		// 	r := readEEPROM(id, scEEPROM, port)
+		// 	if r == nil {
+		// 		continue
+		// 	}
+		// 	ee, err := eeprom.Parse(r)
+		// 	if err != nil {
+		// 		log.Fatalf("eeprom.Parse: %+v", err)
+		// 	}
+		// 	ee.SignalSpeed = eeprom.High
+		// 	compose, err := eeprom.Compose(r, ee)
+		// 	if err != nil {
+		// 		log.Fatalf("eeprom.Compose: %+v", err)
+		// 	}
+		// 	time.Sleep(time.Second)
+		// 	writeEEPROM(id, scEEPROM, compose, port)
+		// 	time.Sleep(time.Second)
+		// }
+
 	}
 
 }
@@ -105,12 +135,15 @@ func readEEPROM(id uint8, sc subCommand, port io.ReadWriteCloser) []byte {
 	b := []byte{cmd, uint8(sc)}
 	fmt.Println(printHex(b))
 	result := writeAndRead(port, b)
-	e, err := eeprom.Parsing(result[2:])
-	if err != nil {
-		log.Fatalf("eeprom.Parsing: %+v", err)
+	if len(result) < 2 {
 		return nil
 	}
-	fmt.Printf("%+v\n", e)
+	_, err := eeprom.Parse(result[2:])
+	if err != nil {
+		log.Printf("eeprom.Parsing: %+v\n", err)
+		return nil
+	}
+	// fmt.Printf("%+v\n", e)
 
 	// save data
 	// testingFilePath := "./Ignore/testing"
@@ -126,13 +159,13 @@ func readEEPROM(id uint8, sc subCommand, port io.ReadWriteCloser) []byte {
 	// 	println(n)
 	// 	f.Close()
 	// }
-	return result
+	return result[2:]
 }
 func trySetPosition(port io.ReadWriteCloser) {
 	const (
 		targetEnd   uint  = 8000
 		targetStart uint  = 7000
-		count       uint  = 10
+		count       uint  = 4
 		step              = (targetEnd - targetStart) / count
 		stepTime          = time.Second / 2
 		idStart     uint8 = 0
@@ -151,6 +184,7 @@ func trySetPosition(port io.ReadWriteCloser) {
 		// fmt.Printf("% 08b, % 08b\n", result.PosH, result.PosL)
 		for i := idStart; i <= idEnd; i++ {
 			setPosition(i, target, port)
+			time.Sleep(time.Millisecond * 10)
 		}
 		// fmt.Println("Read", data[:i])
 		time.Sleep(stepTime)
@@ -161,6 +195,10 @@ func setPosition(id uint8, target uint, port io.ReadWriteCloser) {
 	cmd := byte(0b10000000) + id
 	b := []byte{cmd, position.PosH, position.PosL}
 	result := writeAndRead(port, b)
+	if len(result) != 3 {
+		log.Println("Waring this return data is worng")
+		return
+	}
 	tchH := result[1]
 	tchL := result[2]
 	r := convert.Position{PosH: tchH, PosL: tchL}
@@ -176,7 +214,7 @@ func tryFree(port io.ReadWriteCloser) {
 		tchL := result[2]
 		r := convert.Position{PosH: tchH, PosL: tchL}
 		fmt.Printf("ID: %d, Current: %d\n", id, r.PosToUint())
-		time.Sleep(time.Second)
+		time.Sleep(time.Millisecond * 200)
 	}
 }
 func writeAndRead(port io.ReadWriteCloser, b []byte) []byte {
@@ -195,7 +233,7 @@ func writeAndRead(port io.ReadWriteCloser, b []byte) []byte {
 	}
 	if readN > len(b) {
 		if !bytes.Equal(data[:len(b)], b) {
-			println("[self] Dose'nt Equal!!")
+			log.Println("[self] Dose'nt Equal!!")
 		}
 	}
 
